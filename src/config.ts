7ee9c9293bcf9c02b3177f157365d9a6ffa4b84c
AradTenenbaum <arad.tenen@gmail.com>
2023-10-03 18:38:01 +0300

    small fix

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,6 @@ config();
 
 export const PORT = process.env.PORT;
 export const CLIENT_URL = process.env.CLIENT_URL;
-export const JWT_KEY = process.env.JWT_KEY;
 
 export const DB_PORT = process.env.DB_PORT;
 export const DB_HOST = process.env.DB_HOST;
